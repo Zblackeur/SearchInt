62091afc684beeff1140402907e600175278009a
Zblackeur <cedriczambo2412@gmail.com>
2023-07-28 13:38:27 +0100

    configuration pour le deploiement

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  /*url:"prod-api.com",
+  firebaseConfig: {
+    apiKey: "VOTRE_API_KEY",
+    authDomain: "VOTRE_AUTH_DOMAIN",
+    projectId: "VOTRE_PROJECT_ID",
+    storageBucket: "VOTRE_STORAGE_BUCKET",
+    messagingSenderId: "VOTRE_MESSAGING_SENDER_ID",
+    appId: "VOTRE_APP_ID"
+  }*/
+};
